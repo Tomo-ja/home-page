92b8bf41d725d08572aa7fdff5366819f37f7664
Tomo-ja <tomonao.hj@gmail.com>
2022-07-16 16:47:07 -0700

    design contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,6 +10,43 @@ export default function Contact() {
 			bgImg={headerBgImg}
 			content={<Header title="Contact" subTitle="Hey Hi!" />}
 		/>
+		<div className="inner">
+			<div className="contact__formBox">
+				<form className='contact__form'>
+					<div className='contact__form__group'>
+						<input 
+							type="text" 
+							className='contact__form__input'
+							id='guestName'
+							required 
+						/>
+						<span className='contact__form__border'></span>
+						<label for="guestName" className='contact__form__label'>How can I call you?</label>
+					</div>
+					<div className='contact__form__group'>
+						<input 
+							type="text" 
+							className='contact__form__input'
+							id='guestEmail'
+							required 
+						/>
+						<span className='contact__form__border'></span>
+						<label for="guestEmail" className='contact__form__label'>What's your email address?</label>
+					</div>
+					<div className='contact__form__group'>
+						<textarea 
+							id='message'
+							className='contact__form__input'
+							rows="6"
+							required
+						/>
+						<span className='contact__form__border'></span>
+						<label for="message" className='contact__form__label'>What's your message?</label>
+					</div>
+					<input type="submit" value="Send!" />
+				</form>
+			</div>
+		</div>
 	</section>
   )
 }
